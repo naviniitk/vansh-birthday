d9d436d47a3815209ba2f2c6c127a947f727221c
Naveen Kumar <navin10102001@gmail.com>
2021-07-12 02:36:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const container = document.getElementById('container');
+
+for(let i = 0; i < 16; i+=1){
+  const image = document.createElement('img');
+  const imgContainer = document.createElement('div');
+  imgContainer.setAttribute('class', 'img-container');
+  image.setAttribute('class', 'image');
+  image.src = '/images/pic' + (i + 1) + '.jpg'
+  imgContainer.appendChild(image);
+  container.appendChild(imgContainer);
+
+}
+
