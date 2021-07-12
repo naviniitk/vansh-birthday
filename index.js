b59c1c25a3ee00e4b78293a094c81f51e3b73414
Naveen Kumar <navin10102001@gmail.com>
2021-07-12 10:39:38 +0530

    images

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ for(let i = 0; i < 16; i+=1){
   const imgContainer = document.createElement('div');
   imgContainer.setAttribute('class', 'img-container');
   image.setAttribute('class', 'image');
-  image.src = '/images/pic' + (i + 1) + '.jpg'
+  image.src = 'images/pic' + (i + 1) + '.jpg'
   imgContainer.appendChild(image);
   container.appendChild(imgContainer);
 
